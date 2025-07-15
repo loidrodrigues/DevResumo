a74f01de73d3053933dd828766eb21ebdcfa5556
Loidrodrigues <loidrodrigues5@gmail.com>
2025-07-15 01:42:26 -0300

    style: aplicar cor no titulo do hrader

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,22 +1,12 @@
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Header from "@/src/components/Header";
 import constants from "expo-constants";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   const statusBar = constants.statusBarHeight;
   return (
-    <View style={{ marginTop: statusBar + 40 }} className="mx-8">
-      <View
-        className="flex flex-row  justify-between items-center
-      "
-      >
-        <View className="flex-1">
-          <Text className="text-3xl text-slate-900 font-bold text-center">
-            DevResumo
-          </Text>
-        </View>
-        <MaterialIcons name="settings" size={24} color="black" />
-      </View>
+    <View style={{ marginTop: statusBar + 10 }} className="mx-8">
+      <Header />
     </View>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Text, View } from "react-native";
 export default function Header() {
   return (
-    <View>
-      <Text>DevResumo</Text>
+    <View className="flex flex-row  justify-between items-center  p-8 rounded-lg">
+      <View className="flex-1">
+        <Text className="text-2xl  font-bold text-center">
+          Dev <Text className="text-orange-700 font-normal">Resumo</Text>
+        </Text>
+      </View>
+      <MaterialIcons name="settings" size={24} color="black" />
     </View>
   );
 }
